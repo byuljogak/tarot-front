4ed9448b16baf3e90353264746ccc631d08f0408
sauce <dadad1004@naver.com>
2024-11-27 15:50:02 +0900

    chore: fix touch action element

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,12 +3,13 @@
 import Image from "next/image";
 import Message from "./components/message";
 import { useCallback, useState } from "react";
-import Link from "next/link";
+import { redirect } from "next/navigation";
 
 export default function Home() {
   const messages = [
     "안녕하세요, 타로마스터입니다.",
     "고민이 있으신가요?\n마음을 가다듬고, 질문을 생각해주세요.",
+    "이제 카드를 섞겠습니다. 당신의 오늘에 행운이 깃들길 바랍니다.",
   ];
 
   const [messageIndex, setMessageIndex] = useState(0);
@@ -18,17 +19,14 @@ export default function Home() {
   }, []);
 
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] full-size items-center justify-items-center min-h-screen pt-20 pb-20 gap-16 font-flower-island">
+    <div
+      onClick={
+        messageIndex < messages.length ? nextMessage : redirect("/select")
+      }
+      className="grid grid-rows-[20px_1fr_20px] full-size items-center justify-items-center min-h-screen pt-20 pb-20 gap-16 font-flower-island"
+    >
       <main className="flex flex-col w-full gap-8 row-start-2 items-center">
-        {messageIndex < messages.length ? (
-          <div onClick={nextMessage} onTouchEnd={nextMessage}>
-            <Message message={messages[messageIndex]} />
-          </div>
-        ) : (
-          <Link href={"/select"}>
-            <Message message="이제 카드를 섞겠습니다. 당신의 오늘에 행운이 깃들길 바랍니다." />
-          </Link>
-        )}
+        <Message message={messages[messageIndex]} />
       </main>
       <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
         <a
@@ -39,7 +37,7 @@ export default function Home() {
         >
           <Image
             aria-hidden
-            src="/file.svg"
+            src={"/file.svg"}
             alt="File icon"
             width={16}
             height={16}
@@ -54,7 +52,7 @@ export default function Home() {
         >
           <Image
             aria-hidden
-            src="/window.svg"
+            src={"/window.svg"}
             alt="Window icon"
             width={16}
             height={16}
