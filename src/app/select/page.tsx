d775b65d083926b858f28264078f49be8e1028fd
sauce-Git <dadad1004@naver.com>
2024-11-28 20:12:28 +0900

    feat: hold move action updated

--- src/app/select/page.tsx
+++ src/app/select/page.tsx
@@ -7,96 +7,179 @@ import Card from "@/components/card/card";
 
 export default function Page() {
   const cardCount = 21;
+  const animationDelay = 100;
   const [cardSelected, setCardSelected] = useState(false);
   const [animation, setAnimation] = useState(false);
 
   const cardsRef = useRef<HTMLDivElement>(null);
-  const prevCardRef = useRef<HTMLImageElement>(null);
   const currentCardRef = useRef<HTMLImageElement>(null);
+  const nextCardRef = useRef<HTMLImageElement>(null);
+  const prevCardRef = useRef<HTMLImageElement>(null);
 
   const activateCardSelected = useCallback(() => {
-    if (prevCardRef.current) {
+    if (currentCardRef.current) {
       const keyframes = [
         { transform: "scale(1)" },
         { transform: "scale(1.2)" },
       ];
       const options = {
-        duration: 500,
+        duration: 300,
         easing: "ease-in-out",
       };
       setAnimation(true);
-      prevCardRef.current.animate(keyframes, options);
-      prevCardRef.current.style.transform = "scale(1.2)";
+      currentCardRef.current.animate(keyframes, options);
+      currentCardRef.current.style.transform = "scale(1.2)";
       setTimeout(() => {
         setAnimation(false);
-      }, options.duration);
+      }, options.duration + animationDelay);
     }
     setCardSelected(true);
   }, []);
 
   const deactivateCardSelected = useCallback(() => {
-    if (prevCardRef.current) {
+    if (currentCardRef.current) {
       const keyframes = [
         { transform: "scale(1.2)" },
         { transform: "scale(1)" },
       ];
       const options = {
-        duration: 500,
+        duration: 300,
         easing: "ease-in-out",
       };
       setAnimation(true);
-      prevCardRef.current.animate(keyframes, options);
-      prevCardRef.current.style.transform = "scale(1)";
+      currentCardRef.current.animate(keyframes, options);
+      currentCardRef.current.style.transform = "scale(1)";
       setTimeout(() => {
         setAnimation(false);
-      }, options.duration);
+      }, options.duration + animationDelay);
     }
     setCardSelected(false);
   }, []);
 
-  const moveLeft = useCallback(() => {
-    if (cardsRef.current && prevCardRef.current && currentCardRef.current) {
-      const cardsKeyframes = [
-        { transform: "translateX(0)" },
-        { transform: "translateX(calc(-120px - 1.25rem))" }, // card width + gap
-      ];
-      const prevCardKeyframes = [
-        { transform: cardSelected ? "scale(1.2)" : "cale(1)" },
-        { transform: "scale(1)" },
-      ];
-      const currentCardKeyframes = [
-        { transform: "scale(1)" },
-        { transform: cardSelected ? "scale(1.2)" : "cale(1)" },
-      ];
-      const options = {
-        duration: 500,
-        easing: "ease-in-out",
-      };
-      setAnimation(true);
-      cardsRef.current.animate(cardsKeyframes, options);
-      prevCardRef.current.animate(prevCardKeyframes, options);
-      currentCardRef.current.animate(currentCardKeyframes, options);
-      setTimeout(() => {
-        setAnimation(false);
-      }, options.duration);
-    }
-  }, [cardSelected]);
+  const moveRight = useCallback(
+    (speed: number) => {
+      if (cardsRef.current && currentCardRef.current && nextCardRef.current) {
+        const cardsKeyframes = [
+          { transform: "translateX(0)" },
+          { transform: "translateX(calc(-120px - 1.25rem))" }, // card width + gap
+        ];
+        const prevCardKeyframes = [
+          { transform: cardSelected ? "scale(1.2)" : "scale(1)" },
+          { transform: "scale(1)" },
+        ];
+        const currentCardKeyframes = [
+          { transform: "scale(1)" },
+          { transform: cardSelected ? "scale(1.2)" : "scale(1)" },
+        ];
+        const options = {
+          duration: speed,
+          easing: "ease-in-out",
+        };
+        setAnimation(true);
+        cardsRef.current.animate(cardsKeyframes, options);
+        currentCardRef.current.animate(prevCardKeyframes, options);
+        nextCardRef.current.animate(currentCardKeyframes, options);
+        setTimeout(() => {
+          setAnimation(false);
+        }, options.duration + animationDelay);
+      }
+    },
+    [cardSelected],
+  );
+
+  const moveLeft = useCallback(
+    (speed: number) => {
+      if (cardsRef.current && currentCardRef.current && prevCardRef.current) {
+        const cardsKeyframes = [
+          { transform: "translateX(0)" },
+          { transform: "translateX(calc(120px + 1.25rem))" }, // card width + gap
+        ];
+        const prevCardKeyframes = [
+          { transform: "scale(1)" },
+          { transform: cardSelected ? "scale(1.2)" : "scale(1)" },
+        ];
+        const currentCardKeyframes = [
+          { transform: cardSelected ? "scale(1.2)" : "scale(1)" },
+          { transform: "scale(1)" },
+        ];
+        const options = {
+          duration: speed,
+          easing: "ease-in-out",
+        };
+        setAnimation(true);
+        cardsRef.current.animate(cardsKeyframes, options);
+        prevCardRef.current.animate(prevCardKeyframes, options);
+        currentCardRef.current.animate(currentCardKeyframes, options);
+        setTimeout(() => {
+          setAnimation(false);
+        }, options.duration + animationDelay);
+      }
+    },
+    [cardSelected],
+  );
+
+  const generateMoveInterval = useCallback(
+    (speed: number, clientX: number) => {
+      let interval: NodeJS.Timeout;
+      // move left if clicked on left side of the screen, otherwise move right
+      if (clientX < window.innerWidth / 2) {
+        interval = setInterval(() => {
+          moveLeft(speed);
+        }, speed);
+      } else {
+        interval = setInterval(() => {
+          moveRight(speed);
+        }, speed);
+      }
+
+      return interval;
+    },
+    [moveLeft, moveRight],
+  );
+
+  // hold move
+  const holdMoveClick = useCallback(
+    (speed: number) => (e: React.MouseEvent) => {
+      const clientX = e.clientX;
+      const interval = generateMoveInterval(speed, clientX);
+
+      document.addEventListener("mouseup", () => {
+        clearInterval(interval);
+      });
+    },
+    [generateMoveInterval],
+  );
+
+  const holdMoveTouch = useCallback(
+    (speed: number) => (e: React.TouchEvent) => {
+      const clientX = e.touches[0].clientX;
+      const interval = generateMoveInterval(speed, clientX);
+
+      document.addEventListener("touchend", () => {
+        clearInterval(interval);
+      });
+    },
+    [generateMoveInterval],
+  );
 
   return (
     <div
-      className="grid grid-rows-[1fr_4fr_1fr] items-center w-full pt-20 pb-20 gap-16 font-flower-island overflow-x-hidden"
-      onClick={() => {
+      className="grid grid-rows-[1fr_4fr_1fr] items-center w-full pt-20 pb-20 gap-16 font-flower-island overflow-x-hidden min-h-screen select-none"
+      onClick={(e) => {
         // prevent multiple clicks
-        if (!animation) {
-          // if card is selected, move to the left
-          if (cardSelected) {
-            moveLeft();
+        if (!animation && !cardSelected) {
+          activateCardSelected();
+        } else if (!animation) {
+          const speed = 300;
+          if (e.clientX < window.innerWidth / 2) {
+            moveLeft(speed);
           } else {
-            // if card is not selected, toggle selected
-            activateCardSelected();
+            moveRight(speed);
           }
         }
       }}
+      onMouseDown={holdMoveClick(150)}
+      onTouchStart={holdMoveTouch(150)}
     >
       <div>
         {cardSelected ? (
@@ -105,19 +188,23 @@ export default function Page() {
           <Message message="화면을 눌러 카드를 선택을 시작해주세요." />
         )}
       </div>
-      <div className="flex flex-row flex-center gap-5 py-5" ref={cardsRef}>
-        {Array.from({ length: (cardCount - 1) / 2 }).map((_, index) => (
+      <div
+        className="flex flex-row h-full flex-center gap-5 py-5"
+        ref={cardsRef}
+      >
+        {Array.from({ length: (cardCount - 2) / 2 }).map((_, index) => (
           <Card key={index} width={120} isMini />
         ))}
+        <Card ref={prevCardRef} width={120} isMini />
         <Card
-          ref={prevCardRef}
+          ref={currentCardRef}
           width={120}
           style={{
             transform: cardSelected ? "scale(1.2)" : "scale(1)",
           }}
           isMini
         />
-        <Card ref={currentCardRef} width={120} isMini />
+        <Card ref={nextCardRef} width={120} isMini />
         {Array.from({ length: (cardCount - 2) / 2 }).map((_, index) => (
           <Card key={index} width={120} isMini />
         ))}
@@ -125,7 +212,14 @@ export default function Page() {
       <div className="w-full">
         {cardSelected && (
           <div className="flex flex-row w-full justify-around">
-            <Button href={"/result"}>YES</Button>
+            <Button
+              href={"/result"}
+              onClick={(e) => {
+                e.stopPropagation();
+              }}
+            >
+              YES
+            </Button>
             <Button
               onClick={(e) => {
                 e.stopPropagation();
